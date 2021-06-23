4dc2f81c0dc5d75a4eb33d57f03d96843a427f21
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-06-23 17:49:29 -0500

    Added post endpoint for adding new songs to the data set

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
+
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.listen(5000,function (){
     console.log("Server started. Listening on port 5000.")
@@ -15,4 +19,10 @@ app.get('/api/songs/:id',(req, res) => {
     const id = req.params.id;
     const singleSong = repoContext.songs.findSongById(id);
     return res.send(singleSong);
+})
+
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
 })
