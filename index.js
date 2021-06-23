169a1124af09b2f839dc4ed82e0a6d1ecb2c3154
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-06-23 17:34:51 -0500

    Added get endpoint that pulls a specific song based on id

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.listen(5000,function (){
 });
 
 app.get('/api/songs',(req, res) =>{
-   const songs = repoContext.songs.findAllSongs();
-   return res.send(songs);
+   const allSongs = repoContext.songs.findAllSongs();
+   return res.send(allSongs);
 });
+
+app.get('/api/songs/:id',(req, res) => {
+    const id = req.params.id;
+    const singleSong = repoContext.songs.findSongById(id);
+    return res.send(singleSong);
+})
